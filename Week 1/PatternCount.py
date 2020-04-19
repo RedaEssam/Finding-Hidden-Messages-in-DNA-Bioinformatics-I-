88df9836b02039d559439eeeec8ab7e62d09fea8
RedaEssam <63953805+RedaEssam@users.noreply.github.com>
2020-04-19 22:51:26 +0200

    Update PatternCount.py

--- Week 1/PatternCount.py
+++ Week 1/PatternCount.py
@@ -1,1 +1,6 @@
-
+def PtternCount(Text,Pattern):
+    count = 0
+    for i in range(len(Text)-len(Pattern)+1):
+        if Text[i:i+len(Pattern)]== Pattern:
+            count += 1
+    return count 
